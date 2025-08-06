53073578a9481a52f21b316de36f762b4c8d2f82
shanenelson1117 <shanenelson1117@icloud.com>
2025-08-05 22:22:06 -0700

    write simple testbench

---
+++ source/tl_test.sv
@@ -0,0 +1,37 @@
+`timescale 1ns/10ps
+
+module tl_test;
+
+  logic clk;
+  logic reset;
+
+  parameter ClockDelay = 5000;
+
+  // Instantiate the DUT (Device Under Test)
+  out_of_order dut (
+    .clk(clk),
+    .reset(reset)
+  );
+
+  initial $timeformat(-9, 2, " ns", 10);
+
+  initial begin // Set up the clock
+    clk <= 0;
+    forever #(ClockDelay/2) clk <= ~clk;
+  end
+
+  initial begin
+    $display("Starting single_cycle testbench...");
+
+    reset <= 1; @(posedge clk);
+
+    // Hold reset for a few cycles
+  
+    reset <= 0; @(posedge clk);
+
+    // Run long enough to execute all instructions including the B HALT
+    repeat (200) @(posedge clk);
+    
+  end
+
+endmodule
