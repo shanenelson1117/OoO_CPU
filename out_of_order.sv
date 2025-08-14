d8f7db6575814d96d46d487cc4122e994fbcd274
Shane Nelson <160362521+shanenelson1117@users.noreply.github.com>
2025-08-13 21:17:32 -0700

    Update out_of_order.sv

--- out_of_order.sv
+++ out_of_order.sv
@@ -2,7 +2,7 @@
 // Project: OoO CPU
 // File: Top Level
 
-`include "source/structs.svh"
+`include "structs.svh"
 
 module out_of_order (
     input clk, reset
@@ -12,8 +12,10 @@ module out_of_order (
     // All signals passed between modules
     //--------------------------------------
 
+
+
     // fetch-issue pipeline register packets
-    pipe_in_t pipe_in, pipe_out;  
+    pipe_in_t pipe_in, pipe_out;
     // come from commit unit (branch result and wether or not we commit a branch)
     logic update, valid_in; 
     // from new pc unit
@@ -109,20 +111,23 @@ module out_of_order (
     logic rob_head_ready;
     // from commit to regstat
     logic [3:0] commit_ROB;
+    logic valid_commit, stall_reg, lsq_empty, pc_pipe_stall;
+    logic [31:0] WriteData_reg;
+    CDB_packet_t commit_packet;
 
 
     //--------------------------------------
     // Fetch Stage                         
     //--------------------------------------
-    
 
+    
     // handles pc reads and writes, as well as makes branch predictions
-    fetch fetch_stage (.clk, .reset, .enable(~stall | mispredicted), .update(commit_result), 
-                .valid_in(committed_is_branch), .pc_update, .committed_pc, .pipe_in, .stall, .mispredicted);
+    fetch fetch_stage (.clk(clk), .reset, .enable(~stall | mispredicted), .update(commit_result), 
+                .valid_in(committed_is_branch), .pc_update, .committed_pc, .pipe_in, .stall(pc_pipe_stall), .mispredicted);
     
     // pipeline register between fetch and issue stage
-    pipeline_reg fetch_issue_reg (.clk, .d(pipe_in), .queue_full(stall), 
-                .reset(reset | mispredicted), .q(pipe_out));
+    pipeline_reg fetch_issue_reg (.clk(clk), .d(pipe_in), .queue_full(stall), 
+                .reset(reset | mispredicted), .q(pipe_out), .stall(pc_pipe_stall));
 
     // Generates the next instruction address
     new_pc generate_new_pc (.commit_pc(committed_pc), .commit_imm_se(commit_imm_se), .commit_taken(commit_prediction),
@@ -132,33 +137,34 @@ module out_of_order (
     //--------------------------------------
     // Issue Stage
     //--------------------------------------
-    
 
+    
     // Sends packets to reorder buffer, reservation stations, and load-store queue
     rs_scheduler res_sched (.pipe_out, .busy_bus, .lsq_full, .lsq_input, .rob_full, .rs1reg_busy, .rs2reg_busy, .new_CDB(CDB_out),
-                .rs1_data(rs1reg_data), .rs2_data(rs2reg_data), .curr_branch_imm_se, .Q_j, .Q_k, .rs1, .rs2, .issue_writes,
-                .rs_input, .new_packet(rob_input), .stall, .issue_dest, .ROB_entry, .rs_dest, .clk, .reset(reset | mispredicted));
+                .rs1_data(rs1reg_data), .rs2_data(rs2reg_data), .curr_branch_imm_se, .Q_j, .Q_k, .rs1, .rs2, .issue_writes, .valid_commit,
+                .rs_input, .new_packet(rob_input), .stall, .issue_dest, .ROB_entry, .rs_dest, .clk, .reset(reset | mispredicted),
+                .commit_ROB, .stall_reg, .WriteData, .pc_pipe_stall);
     
     // GP registers
-    regfile registers (.rs1, .rs2, .rd, .RegWrite, .WriteData, .rs1_data(rs1reg_data), .rs2_data(rs2reg_data), .clk, .reset);
-    
+    regfile registers (.rs1, .rs2, .rd, .RegWrite, .WriteData, .rs1_data(rs1reg_data), .rs2_data(rs2reg_data), .clk(clk), .reset);
+
     // Register status registers, keep track of who is writing where, enables register renaming
-    regstat reg_status_register (.rs1, .rs2, .clk, .reset, .issue_writes, .commit_dest(rd), .rs1reg_busy, .rs2reg_busy,
+    regstat reg_status_register (.rs1, .rs2, .clk(clk), .stall(pc_pipe_stall), .reset(reset | mispredicted), .issue_writes, .commit_dest(rd), .rs1reg_busy, .rs2reg_busy,
                 .issue_dest, .RegWrite, .Q_j, .Q_k, .commit_ROB, .issue_ROB(ROB_entry));
     
     // The reservation stations, buffer instructions while they wait for their operands to be calculated
-    rs_module reservation_stations (.stall, .clk, .reset, .mispredicted, .rs_dest, .d(rs_input), .CDB_in(CDB_out), 
+    rs_module reservation_stations (.stall, .clk, .reset(reset | mispredicted), .mispredicted, .rs_dest, .d(rs_input), .CDB_in(CDB_out), 
                     .busy_bus, .consumed_bus, .rs0_data, .rs1_data, .rs2_data, .rs3_data);
-    
-    // Keeps track of the ordering of loads and stores, removes ambiguous memory RAW hazards
+
+    // Keeps track of the ordering of loads and stores, removes ambiguous memory RAW hazards   
     lsq load_store_queue (.clk, .reset(reset | mispredicted), .wr_en, .rd_en, .CDB_in(CDB_out), 
-                    .din(lqss_out), .dout(lsq_out),
+                    .din(lqss_out), .dout(lsq_out), .empty(lsq_empty),
                     .full(lsq_full), .head_ready, .head_load);
 
     // generates right enable signal for lsq
     lsq_scheduler lsq_sched (.in(lsq_input), .out(lqss_out), .wr_en, .lsq_full);
     
-
+    
     //--------------------------------------
     // Execute Stage
     //--------------------------------------
@@ -175,17 +181,17 @@ module out_of_order (
                     .branch_type, .valid_out(valid_out_bus[0]), 
                     .out(out_0), .ALUop(ALU_op), .ready(ready_bus[0]));
 
-    // Adder FU
+    // Adder FU 
     add adder_fu_1 (.clk, .reset(reset | mispredicted), .valid_in(valid_in_bus[1]), .load,
                     .yumi_in(yumi_bus[1]), .rs1(A), .rs2(B), .rs_rob_entry(ROB_entry_fu),
                     .branch_type, .valid_out(valid_out_bus[1]), .out(out_1), 
                     .ALUop(ALU_op), .ready(ready_bus[1]));
 
-    // Multiply FU (booth's algorithm)
+    // Multiply FU (booth's algorithm) 
     multiply mult_fu (.clk, .reset(reset | mispredicted), .A, .B, .rs_rob_entry(ROB_entry_fu), 
                     .yumi_in(yumi_bus[2]), .valid_in(valid_in_bus[2]), .ALUop(ALU_op),
                     .valid_out(valid_out_bus[2]), .ready(ready_bus[2]), .out(out_2));  
-
+    
     // Divide FU (non-restoring division algorithm)
     divide div_fu (.clk, .reset(reset | mispredicted), .valid_in(valid_in_bus[3]), .yumi_in(yumi_bus[3]), 
                     .rs_rob_entry(ROB_entry_fu), .ALUop(ALU_op), .valid_out(valid_out_bus[3]), .ready(ready_bus[3]), 
@@ -194,29 +200,32 @@ module out_of_order (
     // Data Memory FU
     memory data_memory (.clk, .ROB_head_store, .head_load, .head_ready, .mem_in(lsq_out), .mem_read_out(out_load), 
                     .rd_en, .rd_en_rob, .valid_out(valid_out_bus[4]), .reset(reset | mispredicted), 
-                    .yummy_in(yumi_bus[4]));  
+                    .yummy_in(yumi_bus[4]), .empty(lsq_empty));  
+
 
     //--------------------------------------
     // Write Back
     //--------------------------------------
 
-    // Schedules ready packets from functional units for broadcast on cdb to rs, rs scheduler, and rob
+    // Schedules ready packets from functional units for broadcast on cdb to rs, rs scheduler, commit unit, and rob
     cdb_scheduler cdb (.valid_out_bus, .adder_0_out(out_0), .adder_1_out(out_1), .mult_out(out_2), 
                     .div_out(out_3), .mem_out(out_load), .new_CDB(CDB_out), 
-                    .yummi_in_bus(yumi_bus));
-
+                    .yummi_in_bus(yumi_bus), .commit_packet);
+    
+    
     //--------------------------------------
     // Commit
     //--------------------------------------
+
 
     // Queue of instructions. Allows us to only make architectural changes when execution becomes non-speculative
     rob reorder_buffer (.new_entry(rob_input), .CDB_in(CDB_out), .clk, .reset(reset | mispredicted), 
                      .rd_en(rob_read_enable), .empty,
                     .head, .head_ready(rob_head_ready), .full(rob_full), .ROB_head_store, .ROB_entry);
 
+
     // Routes signals of committed instructions to enable architectural changes
     commit commit_unit (.head, .rob_head_ready, .rd_en_rob, .RegWrite, .commit_ROB, .rd, .commit_is_branch(committed_is_branch), 
-                    .commit_prediction, .commit_result, .empty,
+                    .commit_prediction, .commit_result, .empty, .valid_commit, .commit_packet,
                     .WriteData, .committed_pc, .commit_imm_se, .rd_en(rob_read_enable));
-
 endmodule
