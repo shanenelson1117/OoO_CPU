9d074f3abcf0f162b2541310e000faa93003e437
shanenelson1117 <shanenelson1117@icloud.com>
2025-12-23 00:22:53 -0800

    begin fixing circulars / possibly break functionality

--- obj_dir/test_utils.d
+++
@@ -1,7 +0,0 @@
-test_utils.o: ../test/test_utils.cc ../test/../obj_dir/Vcore.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_funcs.h \
-  ../test/test_utils.h
